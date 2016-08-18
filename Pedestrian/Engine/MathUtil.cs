32906a96f264ba5d48ee4952675864664bdcc1c2
Fabien <fabienbrooke@gmail.com>
2016-08-18 03:15:55 -0700

    formatting

--- Pedestrian/Engine/MathUtil.cs
+++ Pedestrian/Engine/MathUtil.cs
@@ -5,17 +5,17 @@ namespace Pedestrian.Engine
 {
     public static class MathUtil
     {
-        static public float AngleBetweenVectors(Vector2 from, Vector2 to)
+        public static float AngleBetweenVectors(Vector2 from, Vector2 to)
         {
             return (float)(Math.Atan2(to.Y, to.X) - Math.Atan2(from.Y, from.X));
         }
 
-        static public float Snap(float value, float increment)
+        public static float Snap(float value, float increment)
         {
             return (float)Math.Round(value / increment) * increment;
         }
 
-        static public bool EqualWithTolerance(float first, float second, float tolerance = 0.001f)
+        public static bool EqualWithTolerance(float first, float second, float tolerance = 0.001f)
         {
             return Math.Abs(first - second) <= tolerance;
         }
