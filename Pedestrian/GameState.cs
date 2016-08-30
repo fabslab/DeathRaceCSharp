584064cec8e6807d53174e9a1447a196964bfff3
Fabien <fabienbrooke@gmail.com>
2016-08-30 02:26:38 -0700

    clean up

--- Pedestrian/GameState.cs
+++ Pedestrian/GameState.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Pedestrian
+﻿namespace Pedestrian
 {
     public enum GameState
     {
